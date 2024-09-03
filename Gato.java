1715a2b59e8194a7ff762b952b05829248e544cf
Rhyan <rhyannsousa@gmail.com>
2024-09-03 20:43:32 -0300

    teste

--- Gato.java
+++ Gato.java
@@ -2,7 +2,7 @@ public class Gato {
     int idade;
     int vidasRestantes;
     String nome;
-
+    float altura;
     public static int quantidadeDeGatos = 0;
     private static final int VIDAS_MAXIMAS = 7;
 
@@ -10,9 +10,18 @@ public class Gato {
         quantidadeDeGatos++;
         vidasRestantes = VIDAS_MAXIMAS;
     }
+
+    /**
+     *Esse método faz o gato miar
+     */
     void miau(){
         System.out.println("Miau");
     }
+
+    /**
+     * Esse método remove uma vida do gato
+     * @author mamae e papai do rhyan
+     */
     void matarGato(){
         if(vidasRestantes==0){
             System.out.println("Já está morto");
--- Main.java
+++ Main.java
@@ -1,17 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
         System.out.println(Gato.quantidadeDeGatos);
 
         Gato novoGato = new Gato();
         novoGato.miau();
-        novoGato.nome = "Gabba";
+        novoGato.nome = scanner.nextLine();
         novoGato.idade = 19;
 
-        System.out.println(Gato.quantidadeDeGatos);
+        novoGato.altura = scanner.nextFloat();
+        System.out.println(novoGato.nome);
 
         System.out.println(novoGato.vidasRestantes);
         novoGato.matarGato();
         System.out.println(novoGato.vidasRestantes);
+
+
+
 
     }
 }
