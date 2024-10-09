6cd2d85d8f6e345becbd3ff04fdb075efc165e1c
Rhyan <rhyannsousa@gmail.com>
2024-10-08 20:21:29 -0600

    Aula 10/09

--- Polinomio/Main.java
+++ Polinomio/Main.java
@@ -1,4 +1,24 @@
 package Polinomio;
 
 public class Main {
+    public static void main(String[] args) {
+        Termo n0 = new Termo(2,1);
+        Termo n1 = new Termo();
+        n1.insere(n0);
+        Termo n2 = new Termo(2,2);
+        Termo n3 = new Termo(2,4);
+        System.out.println(n1.calcula(1));
+        System.out.println(n2.calcula(2));
+        System.out.println(n3.calcula(3));
+        Polinomio p1 = new Polinomio(n1);
+        Polinomio p2 = new Polinomio(n0);
+        System.out.println(p1.calcula(1));
+        System.out.println(p2.calcula(1));
+        p1.insere(n2);
+        p2.insere(n3);
+        System.out.println(p1.calcula(1));
+        System.out.println(p2.calcula(1));
+        p1.fusao(p2);
+        System.out.println(p1.calcula(1));
+    }
 }
--- Polinomio/Polinomio.java
+++ Polinomio/Polinomio.java
@@ -5,18 +5,31 @@ import java.util.ArrayList;
 public class Polinomio {
     ArrayList<Termo> termos = new ArrayList<>();
     Polinomio(Termo objeto){
-        termos.set(objeto.expoente,objeto);
+        termos.add(objeto);
     }
     void insere(Termo objeto){
-        try{
-            termos.get(objeto.expoente);
-            objeto.coeficiente=+termos.get(objeto.expoente).coeficiente;
-            termos.set(objeto.expoente,objeto);
-        } catch (IndexOutOfBoundsException e){
+        int teste = 0;
+        for (int i = 0; i < termos.size(); i++) {
+            if(termos.get(i).expoente== objeto.expoente) {
+                termos.get(i).coeficiente = +objeto.coeficiente;
+                teste = 1;
+                continue;
+            }
+        }
+        if (teste==0) {
             termos.add(objeto);
         }
     }
-    //int calcula(int x){
-
-    //}
+    int calcula(int x){
+        int n=0;
+        for (int i = 0; i < termos.size(); i++) {
+            n=+(int)((int)termos.get(i).coeficiente*Math.pow(x,termos.get(i).expoente));
+        }
+        return n;
+    }
+    void fusao(Polinomio p2){
+        for (int i = 0; i < p2.termos.size(); i++) {
+            insere(p2.termos.get(i));
+        }
+    }
 }
